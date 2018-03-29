e63a51cccdafc971f2dd8037de3dee7cd53d0d76
cesquivel101 <esquivel101@gmail.com>
2018-03-29 14:26:04 -0600

    - B funcional (solo para 1 ejemplo)

--- TareaProgramada_MPI.c
+++ TareaProgramada_MPI.c
@@ -12,11 +12,16 @@ int main(int argc,char **argv)
 	//Every subprocess uses this to do its calculations
 	int* sendcounts;
 	int* displs;
+	int* recvcounts;
+	int* recvdispls;
 	int* sub_matriz_m;
+	int* sub_matriz_b;
 	MPI_Status  status;        /* return status para receptor  */
 	int         tag = 0;       /* etiqueta para mensajes */
 	int numberOfIntsToAssignToSendCounts;
 	int rows_sub_matriz_m;
+	int* b;
+	
 	
 	
 	MPI_Init(&argc,&argv);
@@ -24,6 +29,7 @@ int main(int argc,char **argv)
     MPI_Comm_size(MPI_COMM_WORLD,&numProcs);
  
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+	
 
 	if (myid == 0)
 	{
@@ -47,14 +53,18 @@ int main(int argc,char **argv)
 
 		sendcounts = (int *)malloc(sizeof(int)*numProcs);
 		displs = (int *)malloc(sizeof(int)*numProcs);
+		recvcounts = (int *)malloc(sizeof(int)*numProcs);;
+		recvdispls = (int *)malloc(sizeof(int)*numProcs);;
 		
 		int i = 0;
-		
+			
 		for(i = 0; i < numProcs;i++)
 		{
 			sendcounts[i] = numberOfIntsToAssignToSendCounts;
-			
+			recvcounts[i] = numberOfIntsToAssignToSendCounts;
+			recvdispls[i] = i*((n*n)/numProcs);
 		}
+
 		//MPI_Bcast(sendcounts, numProcs, MPI_INT,myid,MPI_COMM_WORLD);
 		// displs[0] = 0;
 		// displs[numProcs-1] = numProcs*(n/2+1);
@@ -63,7 +73,9 @@ int main(int argc,char **argv)
 		
 		fillDispls(displs,n,numProcs);
 		
-
+		printArray(recvdispls,numProcs);
+		printArray(recvcounts,numProcs);
+		//fprintf(stdout,"tengo el valor %d \n",myid,numberOfIntsToAssignToSendCounts);
 		 //MPI_Scatterv(x, sendcounts, displs, MPI_INT, local_x, 100, MPI_INT, 0, MPI_COMM_WORLD);
 
 		 //MPI_Scatterv(matriz_m, sendcounts, displs, MPI_INT, local_x, 100, MPI_INT, 0, MPI_COMM_WORLD);
@@ -82,46 +94,89 @@ int main(int argc,char **argv)
 	{
 		rows_sub_matriz_m = (n/numProcs)+1;
 	}
+	
+	b = (int *)malloc(n*n * sizeof(int));
+	sub_matriz_b = (int *)malloc(rows_sub_matriz_m*n * sizeof(int));
 	//fprintf(stdout,"ANTES: HOla yo soy %d y tengo el valor %d \n",myid,numberOfIntsToAssignToSendCounts);
 			//ScatterV here...
-	
 	MPI_Scatterv(matriz_m, sendcounts, displs, MPI_INT,sub_matriz_m, numberOfIntsToAssignToSendCounts, MPI_INT,0, MPI_COMM_WORLD);
-	MPI_Barrier(MPI_COMM_WORLD); 
+	
+	calculateCross(sub_matriz_m,rows_sub_matriz_m,n,sub_matriz_b);
+	
+	if(myid != 0)
+	{
+		sub_matriz_b = &sub_matriz_b[n];
+	}
+	else
+	{
+		fprintf(stdout,"mjm cuantos es numberOfIntsToAssignToSendCounts %d\n",numberOfIntsToAssignToSendCounts);
+	}
+
+	MPI_Gather(sub_matriz_b, (n*n)/numProcs, MPI_INT, b, (n*n)/numProcs, MPI_INT, 0, MPI_COMM_WORLD);
+
 	if(myid == 0)
 	{
-		fprintf(stdout,"HOla yo soy %d\n",myid);
-		printNonSquareMatrix(sub_matriz_m,rows_sub_matriz_m,15);
-	}
-	MPI_Barrier(MPI_COMM_WORLD); 
-	//Magic calculations...
-	if(myid == 1){
-		fprintf(stdout,"HOla yo soy %d\n",myid);
-		printNonSquareMatrix(sub_matriz_m,rows_sub_matriz_m,15);
-	}
-	MPI_Barrier(MPI_COMM_WORLD); 
-	if(myid == 2){
-		fprintf(stdout,"HOla yo soy %d\n",myid);
-		printNonSquareMatrix(sub_matriz_m,rows_sub_matriz_m,15);
-	}
-	MPI_Barrier(MPI_COMM_WORLD); 
-	if(myid == 0)
-	{
-		//Information gathering probably gatherv...
-		
-		// printMatrix(matriz_m,n);
-		// printf("\n");
-		// printArray(vector_v,n);
-		// printf("displs: ");
-		// printarray(displs,numprocs);
-		// printf("sendcounts: ");
-		// printarray(sendcounts,numprocs);
-		// printf("vector_v: ");
-		// printarray(vector_v,numprocs);
-		printf("matriz_m: ");
-		printMatrix(matriz_m,n);
+		printf("matriz b: ");
+		printMatrix(b,n);
 	}
 	
 	MPI_Finalize();
+}
+
+int isSubtractionLessThanZero(int value)
+{
+	int result = 0;
+	if((value - 1) < 0)
+	{
+		result = 1;
+	}
+	return result;
+}
+
+int isAdditionGreaterThanN(int value,int n)
+{
+	int result = 0;
+	if((value + 1) >= n)
+	{
+		result = 1;
+	}
+	return result;
+}
+
+// in this case, columns is n
+void calculateCross(int* array_n,int rows,int columns,int* sub_matriz_b)
+{
+	int i=0,j=0;
+	//fprintf(stdout,"Calculo de sub matriz b %d x %d \n",rows,columns);
+	for(i = 0; i < rows; i++)
+	{
+		for(j = 0; j < columns ; j++)
+		{
+			sub_matriz_b[i*columns+j] = array_n[i*columns+j];
+			//M[i,j-1]
+			if(isSubtractionLessThanZero(j) == 0)
+			{
+				sub_matriz_b[i*columns+j] += array_n[i*columns+(j-1)];
+			}
+			//M[i-1,j]
+			if(isSubtractionLessThanZero(i) == 0)
+			{
+				sub_matriz_b[i*columns+j] += array_n[(i-1)*columns+j];
+			}
+			//M[i+1,j]
+			if(isAdditionGreaterThanN(i,rows) == 0)
+			{
+				sub_matriz_b[i*columns+j] += array_n[(i+1)*columns+j];
+			}
+			//M[i,j+1]
+			if(isAdditionGreaterThanN(j,columns) == 0 )
+			{
+				sub_matriz_b[i*columns+j] += array_n[i*columns+(j+1)];
+			}
+			//fprintf(stdout,"%d ",array_n[i*columns+j]);
+		}
+		//printf("\n");
+	}
 }
 
 int scatterVLast(int n, int numProcs)
@@ -187,10 +242,10 @@ void fillMatrixAndVector(int * matriz_m,int* vector_v,int n)
 	int i=0,j=0;
 	 for(i = 0; i < n; i++)
 	 {
-		 vector_v[i] = i;//randomNumber(5);//i; //random 0-5
+		 vector_v[i] = i+1;/*randomNumber(5);*//*i; *///random 0-5
 		 for(j = 0; j < n ; j++)
 		{
-			 matriz_m[i*n+j] = j;//randomNumber(10);//j;//random 0-9;
+			 matriz_m[i*n+j] = j+1;/*randomNumber(10);*//*j;*///random 0-9;
 		 }
 	 }
 }
